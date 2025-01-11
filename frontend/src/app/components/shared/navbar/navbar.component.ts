2c39665caad762c5373f814d52aece7b2bda7c95
son-of-mountain <elhansali.mouaad@etu.uae.ac.ma>
2025-01-11 17:24:23 +0100

    fix: navbar to optimization and backup

--- frontend/src/app/components/shared/navbar/navbar.component.ts
+++ frontend/src/app/components/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +6,26 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
 
+  ngOnInit(): void {
+    this.initFixedNavbar();
+  }
+
+  private initFixedNavbar(): void {
+    const fixedTop = document.querySelector('.fixed-top') as HTMLElement;
+    window.addEventListener('scroll', () => {
+      const scrollTop = window.scrollY;
+      if (window.innerWidth < 992) {
+        fixedTop?.classList.toggle('bg-white', scrollTop > 45);
+        fixedTop?.classList.toggle('shadow', scrollTop > 45);
+      } else {
+        fixedTop?.classList.toggle('bg-white', scrollTop > 45);
+        fixedTop?.classList.toggle('shadow', scrollTop > 45);
+        if (fixedTop) {
+          fixedTop.style.top = scrollTop > 6 ? '-6px' : '0';
+        }
+      }
+    });
+  }
 }
