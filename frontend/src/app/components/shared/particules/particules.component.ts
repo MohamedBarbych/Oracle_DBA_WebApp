d629de3b401c7bda16b4c49a38c3973ebd681547
MohamedBarbych <MohamedBarbych@users.noreply.github.com>
2025-01-11 12:08:59 +0100

    Create: new background shared component

---
+++ frontend/src/app/components/shared/particules/particules.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-particules',
+  imports: [],
+  templateUrl: './particules.component.html',
+  styleUrl: './particules.component.css'
+})
+export class ParticulesComponent {
+
+}
